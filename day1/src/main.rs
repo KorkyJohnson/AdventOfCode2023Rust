25e22fad7e08657ba86da577210d2b45b65f70d7
Robert Hanna <rdhanna@gmail.com>
2023-12-02 16:04:58 -0500

    start of day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let text = String::from("asdd5dadd5dfff5eeee9ddff");
+
+    let mut start_num:i32 = 0;
+    let mut last_num: i32 = 0;
+
+    for (i, c) in text.chars().enumerate(){
+        if c.is_digit(10){
+            start_num = c.to_digit(10).unwrap_or(0) as i32;
+            break;
+        }
+    }
+    
+    for (i, c) in text.char_indices().rev(){
+        if c.is_digit(10){
+            last_num= c.to_digit(10).unwrap_or(0) as i32;
+            break;
+        }
+    }
+
+    println!("Start num: {} Last Num: {}", start_num, last_num)
 }
